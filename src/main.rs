c5c6ec435b56267f7bddeb5c9d62ee2e1dfa47d3
J2ghz <j2.00ghz@gmail.com>
2020-06-15 23:37:48 +0200

    Use latest file from dir

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
+use futures::stream::{self, StreamExt};
 use prometheus_exporter_base::render_prometheus;
 use prometheus_jfplaybackreporting_exporter::{read_file, render};
 use std::{env, path::Path};
+use tokio::fs::read_dir;
+use tokio::prelude::*;
 
 #[tokio::main]
 async fn main() {
@@ -8,9 +11,31 @@ async fn main() {
         ([0, 0, 0, 0], 32221).into(),
         {},
         |_request, _options| async move {
-            let path_arg = env::args().into_iter().nth(1).unwrap();
+            let path_arg = env::args()
+                .into_iter()
+                .nth(1)
+                .expect("Supply path to a directory");
             let path = Path::new(&path_arg);
-            let reader = read_file(&path);
+            let dir = read_dir(path).await.expect("Couldn't read directory");
+            let (file, _) = dir
+                .fold(None, |acc, it| async {
+                    let file = it.expect("Couldn't read file info");
+                    let meta = file.metadata().await.expect("Couldn't read file metadata");
+                    let modif = meta.modified().expect("Couldn't read file's modified date");
+                    if let Some((path, date)) = acc {
+                        if date > modif {
+                            Some((path, date))
+                        } else {
+                            Some((file.path(), modif))
+                        }
+                    } else {
+                        Some((file.path(), modif))
+                    }
+                })
+                .await
+                .expect("No files found");
+            eprintln!("Reading {}", file.display());
+            let reader = read_file(&file);
             Ok(render(reader))
         },
     )
